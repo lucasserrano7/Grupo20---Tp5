7d007324702cab1a701459a566d17ed3b56f4af5
Lucas <serranolucas1000@gmail.com>
2026-06-08 17:21:29 -0300

    actullizacion conversor

--- Grupo20/src/main/java/com/mycompany/mavenproject2/Conversor.java
+++ Grupo20/src/main/java/com/mycompany/mavenproject2/Conversor.java
@@ -1,64 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.mavenproject2;
+
 import java.text.DecimalFormat;
-/**
- *
- * @author angel
- */
+
 public class Conversor {
+
     double pesos;
     double dolar;
     double saldo;
-    
+
     public Conversor() {
     }
-    
 
-public static double convertirADolar(double pesos, double dolar){
-    
-    double resultado = pesos/dolar;
-    
-    return resultado;
-    
-}   
-
-public static String convertirAPesos(double dolar, double cotizacion){
-    
-    double resultado = dolar*cotizacion;
-    DecimalFormat df = new DecimalFormat("$ #,##0");
-    String resultado2 = df.format(resultado); 
-    
-    return resultado2;
-    
-}   
-
-public static double calcularCotizacion(double pesos, double dolar){
-    
-      if(pesos == 0){
-        throw new ArithmeticException("Los pesos no pueden ser cero");
+    public static double convertirADolar(double pesos, double dolar) {
+        return pesos / dolar;
     }
-    
-    double resultado= pesos/dolar;
-        
-        
-    return resultado;
-}
 
-public static double actualizarSaldo(double saldo, double nuevoMonto){
-    double resultado = saldo + nuevoMonto;
-    
-    return resultado;
-}
+    public static String convertirAPesos(double dolar, double cotizacion) {
 
-public static double retirarMonto(double saldo, double monto){
-    double resultado = saldo - monto;
-    
-    return resultado;
-}
-   
+        double resultado = dolar * cotizacion;
+        DecimalFormat df = new DecimalFormat("$ #,##0");
+        String resultado2 = df.format(resultado);
+
+        return resultado2;
+
+    }
+
+    public static double calcularCotizacion(double pesos, double dolar) {
+
+        if (pesos == 0) {
+            throw new ArithmeticException("Los pesos no pueden ser cero");
+        }
+
+        double resultado = pesos / dolar;
+
+        return resultado;
+    }
+
+    public static double actualizarSaldo(double saldo, double nuevoMonto) {
+        return saldo + nuevoMonto;
+    }
+
+    public static double retirarMonto(double saldo, double monto) {
+        return saldo - monto;
+    }
+
 }
 /*Realizar la clase java Conversor de Moneda que permite por el
 momento, realizar las cuatro operaciones convertir una moneda en
