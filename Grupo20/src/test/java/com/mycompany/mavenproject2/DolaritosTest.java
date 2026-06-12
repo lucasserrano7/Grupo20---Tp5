0f9f0d0b9679d0cd1d607a027e1e1b73c57a3835
Nico-ceo <teito.bris@gmail.com>
2026-06-12 11:56:44 -0300

    Dolaritostest
    cambie de lugar su ubicacion

--- Grupo20/src/test/java/com/mycompany/mavenproject2/DolaritosTest.java
+++ Grupo20/src/test/java/com/mycompany/mavenproject2/DolaritosTest.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
-package Dolares;
+package com.mycompany.mavenproject2;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
@@ -10,35 +10,41 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 /**
  *
  * @author teito
  */
 public class DolaritosTest {
-    
-    public DolaritosTest() {
+
+    @ParameterizedTest
+    @CsvSource({
+        "180000,1500,120",
+        "100000,-900,-111.11"
+    })
+    public void testCalcularCotizacion(double pesos,
+            double dolar,
+            double esperado) {
+
+        double resultado = Conversor.calcularCotizacion(pesos, dolar);
+
+        assertEquals(esperado, resultado, 0.01);
     }
-    
+
     @BeforeAll
     public static void setUpClass() {
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
+
+// TODO add test methods here.
+// The methods must be annotated with annotation @Test. For example:
+//
+// @Test
+// public void hello() {}
+
